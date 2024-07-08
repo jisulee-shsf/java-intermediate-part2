07597fc8af3ccabe18b7485211984abe7f9904db
Jisu Lee <jisulee.shsf@gmail.com>
2024-07-08 23:08:17 +0900

    와일드카드 - 상한 및 하한 와일드카드

---
+++ src/generic/test/ex5/WildcardExMain2.java
@@ -0,0 +1,24 @@
+package generic.test.ex5;
+
+import generic.animal.Animal;
+import generic.animal.Cat;
+import generic.animal.Dog;
+
+public class WildcardExMain2 {
+
+    public static void main(String[] args) {
+        Box<Object> objectBox = new Box<>();
+        Box<Animal> animalBox = new Box<Animal>();
+        Box<Dog> dogBox = new Box<>();
+        Box<Cat> catBox = new Box<>();
+
+        writeBox(objectBox);
+        writeBox(animalBox);
+//        writeBox(dogBox);
+//        writeBox(catBox);
+    }
+
+    public static void writeBox(Box<? super Animal> box) {
+        box.set(new Dog("멍이", 100));
+    }
+}
