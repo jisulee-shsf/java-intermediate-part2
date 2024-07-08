1dd98c757f63a760cca49900b36cffd4e593c78a
Jisu Lee <jisulee.shsf@gmail.com>
2024-07-08 23:36:18 +0900

    타입 이레이저

---
+++ src/generic/test/ex5/EraserBox.java
@@ -0,0 +1,12 @@
+package generic.test.ex5;
+
+public class EraserBox<T> {
+
+//    public boolean instanceCheck(Object obj) {
+//        return obj instanceof T;
+//    }
+
+    public void create() {
+//        return new T();
+    }
+}
